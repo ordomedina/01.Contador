0bd81f4c8d9c9f76f22e5cfc284642392729c9d3
ordomedina <ordomedina@gmail.com>
2020-06-24 16:24:56 +0200

    Vídeo 15

--- app/src/main/java/com/example/contador/MainActivity.java
+++ app/src/main/java/com/example/contador/MainActivity.java
@@ -33,8 +33,8 @@ public class MainActivity extends Activity {
     }
 
     public void mostrarResultado(){
-        TextView textoResultado = (TextView) findViewById(R.id.contadorPulsaciones);
-        textoResultado.setText("Contador: " + contador);
+        TextView textoResultado = (TextView) findViewById(R.id.contadorTexto);
+        textoResultado.setText(""+contador);
     }
 
 

c29ab7ef7244e026cc9b88c42be94d5ca6de2574
ordomedina <ordomedina@gmail.com>
2020-06-25 15:07:13 +0200

    Vídeo 18

--- app/src/main/java/com/example/contador/MainActivity.java
+++ app/src/main/java/com/example/contador/MainActivity.java
@@ -3,17 +3,23 @@ package com.example.contador;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
 
     public int contador;
+    TextView textoResultado;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textoResultado = (TextView) findViewById(R.id.contadorTexto);
         contador= 0;
     }
 
@@ -23,21 +29,27 @@ public class MainActivity extends Activity {
     }
 
     public void decrementaContador(View vista){
+        CheckBox negativos = (CheckBox) findViewById(R.id.checkBox);
         contador--;
+        if(contador<0 && !negativos.isChecked()){
+                contador = 0;
+        }
         mostrarResultado();
     }
 
     public void reseteaContador(View vista){
-        contador = 0;
+        EditText numReset= (EditText) findViewById(R.id.reseteo);
+        try{
+            contador = Integer.parseInt(numReset.getText().toString());
+        }catch (Exception e){
+            contador = 0;
+        }
+        numReset.setText("");
         mostrarResultado();
     }
 
-    public void mostrarResultado(){
+   public void mostrarResultado(){
         TextView textoResultado = (TextView) findViewById(R.id.contadorTexto);
         textoResultado.setText(""+contador);
     }
-
-
-
-
 }
